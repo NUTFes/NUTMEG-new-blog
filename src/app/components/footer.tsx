d22fb84aad5998f243089200a0e7336b40e7738f
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-07-07 23:06:00 +0900

    [feat]SNSリンクを作成

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -7,13 +7,47 @@ const Footer = () => {
     <footer>
       <div className="footer-contents">
         <div className="menu">
-          <Image src="/logo_footer.png" alt="NUTMEG" height={72} width={225} />
-          <div className="links">
-            <Link href="/">Home</Link>
-            <Link href="/teams-projects">Teams & Projects</Link>
-            <Link href="/blogs">Blogs</Link>
-            <Link href="/members">Members</Link>
-            <Link href="/contact">Contact</Link>
+          <div className="column1">
+            <Image
+              src="/logo_footer.png"
+              alt="NUTMEG"
+              height={72}
+              width={225}
+            />
+            <div className="links">
+              <Link href="/">Home</Link>
+              <Link href="/teams-projects">Teams & Projects</Link>
+              <Link href="/blogs">Blogs</Link>
+              <Link href="/members">Members</Link>
+              <Link href="/contact">Contact</Link>
+            </div>
+          </div>
+          <div className="links-sns">
+            {/* リンク先は適宜追加してください */}
+            <a href="">
+              <Image
+                src="/footer_icons/footer_x.png"
+                alt="x"
+                width={25}
+                height={25}
+              />
+            </a>
+            <a href="">
+              <Image
+                src="/footer_icons/footer_instagram.png"
+                alt="instagram"
+                width={25}
+                height={25}
+              />
+            </a>
+            <a href="">
+              <Image
+                src="/footer_icons/footer_github.png"
+                alt="github"
+                width={25}
+                height={25}
+              />
+            </a>
           </div>
         </div>
         <div className="address">〒940-2188 新潟県長岡市上富岡町1603-1</div>
