279d1221db572260559b9dbfd3c802cd08b74310
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-07-07 22:43:06 +0900

    [feat]住所を追加

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -5,15 +5,18 @@ import "./footer.css"; // CSS ファイルをインポート
 const Footer = () => {
   return (
     <footer>
-      <div className="menu">
-        <Image src="/logo_footer.png" alt="NUTMEG" height={72} width={225} />
-        <div className="links">
-          <Link href="/">Home</Link>
-          <Link href="/teams-projects">Teams & Projects</Link>
-          <Link href="/blogs">Blogs</Link>
-          <Link href="/members">Members</Link>
-          <Link href="/contact">Contact</Link>
+      <div className="footer-contents">
+        <div className="menu">
+          <Image src="/logo_footer.png" alt="NUTMEG" height={72} width={225} />
+          <div className="links">
+            <Link href="/">Home</Link>
+            <Link href="/teams-projects">Teams & Projects</Link>
+            <Link href="/blogs">Blogs</Link>
+            <Link href="/members">Members</Link>
+            <Link href="/contact">Contact</Link>
+          </div>
         </div>
+        <div className="address">〒940-2188 新潟県長岡市上富岡町1603-1</div>
       </div>
     </footer>
   );
