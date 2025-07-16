282ddeb1c5a2ad26de1274eac3ffc2e0eb019c77
uchida189 <130959298+uchida189@users.noreply.github.com>
2025-07-16 14:38:06 +0900

    [feat]ブログページの叩き台作成

--- src/app/home/blogs/page.tsx
+++ src/app/home/blogs/page.tsx
@@ -1,11 +1,25 @@
-// pages/index.tsx
 import React from "react";
 import Layout from "../../components/layout";
+import BlogList from "../../components/BlogList";
+import { getBlogPosts } from "../../../lib/notion";
 
-const Blogs = () => {
+const Blogs = async () => {
+  // ブログ記事を取得
+  const posts = await getBlogPosts();
+
   return (
     <Layout>
-      <p>ブログページです</p>
+      <div className="mt-16 px-32 py-8 text-left">
+        <h1 className="text-3xl font-bold text-gray-900 mb-2">
+          Blogs
+        </h1>
+        <p className="text-gray-900 mb-8">
+          NUTMEGでは、外部の方へ活動の様子を伝えることと、文面でのアウトプットを目的として、定期的にメンバーがブログの更新を行っています。
+        </p>
+        
+        {/* ブログ記事の一覧 */}
+        <BlogList posts={posts} />
+      </div>
     </Layout>
   );
 };
