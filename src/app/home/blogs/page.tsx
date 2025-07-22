98d71ada696ba3a5cb641d3fbc99e6e6c945f585
uchida189 <130959298+uchida189@users.noreply.github.com>
2025-07-22 22:39:46 +0900

    [fix]コンポーネントをフォルダに分割

--- src/app/home/blogs/page.tsx
+++ src/app/home/blogs/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Layout from "../../components/layout";
-import BlogList from "../../components/BlogList";
-import { getBlogPosts } from "../../../lib/notion";
+import Layout from "@/app/components/layout";
+import BlogList from "@/app/components/BlogList/BlogList";
+import { getBlogPosts } from "@/lib/notion"; // ブログ記事を取得する関数
 
 const Blogs = async () => {
   // ブログ記事を取得
