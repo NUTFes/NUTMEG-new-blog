1e2d19810bfc2f7aa4e43d0d40a2bd6d71cdeba6
mugiiicha <s243346@stn.nagaokaut.ac.jp>
2025-08-31 20:36:44 +0900

    ブログページの作成見ためは途中

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,28 +1,29 @@
 import React from "react";
-// import Layout from "@/app/layout";
-import Layout from "../layout";
-// import BlogList from "@/app/components/BlogList/BlogList";
+import styles from "./page.module.css";
 import BlogList from "../components/BlogList/BlogList";
-// import { getBlogPosts } from "@/lib/notion"; // ブログ記事を取得する関数
-import { getBlogPosts } from "../../app/lib/notion"; // ブログ記事を取得する関数
+import { getBlogPosts } from "../../app/lib/notion";
+import Link from "next/link";
+
 const Blogs = async () => {
-  // ブログ記事を取得
   const posts = await getBlogPosts();
 
   return (
-    // <Layout>
-      <div className="mt-16 px-32 py-8 text-left">
-        <h1 className="text-3xl font-bold text-gray-900 mb-2">
-          Blogs
-        </h1>
-        <p className="text-gray-900 mb-8">
-          NUTMEGでは、外部の方へ活動の様子を伝えることと、文面でのアウトプットを目的として、定期的にメンバーがブログの更新を行っています。
-        </p>
-        
-        {/* ブログ記事の一覧 */}
-        <BlogList posts={posts} />
+    <div className={styles.container}>
+      
+      <div className={styles.title}>
+        <Link href="/" className={styles.breadcrumb}>Home</Link>
+        <span className={styles.separator}>&gt;</span>
+        <Link href="/blogs" className={styles.heading}>Blogs</Link>
       </div>
-    // </Layout>
+
+
+      <p className={styles.description}>
+        NUTMEGでは、外部の方へ活動の様子を伝えることと、文面でのアウトプットを目的として、
+        定期的にメンバーがブログの更新を行っています。
+      </p>
+
+      <BlogList posts={posts} />
+    </div>
   );
 };
 
