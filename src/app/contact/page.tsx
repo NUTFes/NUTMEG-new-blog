0780895cb15866b5a0ee5917a86bc3b83626ebc5
y2-yue <64639409+y2-yue@users.noreply.github.com>
2025-10-31 05:13:43 +0900

    [feat] コンタクトページのフロント部分を作成。細かい部分は要修正。

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,37 @@
-// pages/index.tsx
-import React from "react";
+"use client";
 
-const Contact = () => {
+import React from "react";
+import Form from "next/form";
+import { postAction } from "@/app/contact/postAction";
+import styles from '@/app/contact/contact.module.css';
+
+export default function ContactPage() {
   return (
     <>
-      <p>コンタクトページです</p>
+      <header className={styles.contactHeader}>
+        <h1 className="text-4xl font-extrabold tracking-tight leading-tight">Contact</h1>
+        <p>お問い合わせ</p>
+      </header>
+
+      <div className={styles.contactContainer}>
+        <Form action={postAction} className={styles.contactForm}>
+          <div className="userName">
+            <label htmlFor="name">お名前または団体者名</label>
+            <input type="text" id="name" name="name" placeholder="お名前" />
+          </div>
+          <div className="userEmail">
+            <label htmlFor="email">ご連絡先</label>
+            <input type="text" id="email" name="email" placeholder="ご連絡先" />
+          </div>
+          <div className="userInquiry">
+            <label htmlFor="inquiry">お問い合わせ内容</label>
+            <textarea id="inquiry" name="inquiry" placeholder="お問い合わせ内容をご記入ください" />
+          </div>
+          <div className="contactFormSubmit">
+            <button type="submit" className={styles.submitButton}>送信</button>
+          </div>
+        </Form>
+      </div>
     </>
   );
-};
-
-export default Contact;
+}
---
+++ src/app/contact/postAction.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export async function postAction(formData: FormData) {
+  // Validation, 送信確認等をここでおこなう
+}
